2c8303593563602086933d96059a78fa71842fae
CioazaIoan <cioazai@yahoo.com>
2025-09-13 19:39:44 +0300

    primul

---
+++ script.js
@@ -0,0 +1,24 @@
+// Verificarea mai multor condiții:
+
+let a = 10;
+let b = 20;
+
+console.log(a > 5 && b < 30);
+console.log(a > 15 && b < 30);
+
+// Verificarea condiției cu variabile boolean:
+
+let isAdult = true;
+let hasID = false;
+
+console.log(isAdult && hasID);
+
+// Confirmă dacă o idee este adevărată pentru ambele variabile:
+
+let temperatura = 25;
+let vreme = "plăcut";
+
+console.log(temperatura >= 20 && vreme === "plăcut");
+
+// Condiție complexă:
+// nu inteleg conditie complexa
