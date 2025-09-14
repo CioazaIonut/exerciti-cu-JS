748a1971e0f477390c4bccc4ffc3927297d082f4
CioazaIoan <cioazai@yahoo.com>
2025-09-14 07:28:07 +0300

    doi

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ console.log(temperatura >= 20 && vreme === "plăcut");
 
 // Condiție complexă:
 // nu inteleg conditie complexa
+
+// Verifică dacă două condiții sunt adevărate:
+
+let var1 = 15;
+let var2 = 30;
+console.log(var1 > 10 && var2 < 40);
+
+// Verifică dacă ambele variabile boolean sunt adevărate:
+
+let areAshi = true;
+let areBatoane = false;
+console.log(areAshi && areBatoane);
+
+// Într-un scenariu de login:
+
+let username = "admin";
+let parolaCorecta = true;
+
+console.log(username === "admin" && parolaCorecta);
+
+// Condiție cu valori numerice:
+
+let temperatura = 22;
+let umiditate = 50;
+
+console.log(temperatura >= 20 && umiditate < 60);
+
+// Verificare dacă ambele variabile sunt diferite de zero:
+
+let x = 5;
+let y = 10;
